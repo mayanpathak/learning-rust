8162009d42414a2d723f0a479da21958c868b7a9
Mayan Pathak <ranupathak1986@gmail.com>
2025-09-20 19:31:05 +0530

    add

---
+++ textind/target/debug/deps/textind-57ad317eb6783b64.d
@@ -0,0 +1,5 @@
+C:\Users\Mayan\Desktop\rust 30 days\textind\target\debug\deps\textind-57ad317eb6783b64.d: src\main.rs
+
+C:\Users\Mayan\Desktop\rust 30 days\textind\target\debug\deps\libtextind-57ad317eb6783b64.rmeta: src\main.rs
+
+src\main.rs:
---
+++ textind/target/debug/deps/textind-cf24c2c7c6663313.d
@@ -0,0 +1,5 @@
+C:\Users\Mayan\Desktop\rust 30 days\textind\target\debug\deps\textind-cf24c2c7c6663313.d: src\main.rs
+
+C:\Users\Mayan\Desktop\rust 30 days\textind\target\debug\deps\libtextind-cf24c2c7c6663313.rmeta: src\main.rs
+
+src\main.rs:
