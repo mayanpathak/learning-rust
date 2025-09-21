7d0a2f74e5fa5890688d631278259a8e4409f7fe
Mayan Pathak <ranupathak1986@gmail.com>
2025-09-21 18:49:04 +0530

    second exercise

--- enums/src/main.rs
+++ enums/src/main.rs
@@ -13,3 +13,28 @@
 //         Direction::West => println!("Going West!"),
 //     }
 // }
+
+
+// enum Message{
+//     Quit,
+//     Move {x: i32, y: i32},
+//     Write(String),
+//     ChangeColour(u8, u8, u8),
+// }
+
+
+// fn Handle (msg: Message){
+//     match msg {
+//         Message::Quit => println!("quit"),
+//         Message::Move {x,y} => println!("Move to ({}, {})", x, y),
+//          Message::Write(s) => println!("Write: {}", s),
+//         Message::ChangeColour(r, g, b) => println!("Color: #{:02x}{:02x}{:02x}", r, g, b)
+//     }
+// }
+// fn main (){
+//     let answer = Message::Move {x: 10, y: 20};
+//     Handle(answer);
+// }
+
+
+
