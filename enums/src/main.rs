176687de756634251b01420c03b44c07cb9d53b5
Mayan Pathak <ranupathak1986@gmail.com>
2025-09-21 18:43:14 +0530

    enum exercises

---
+++ enums/src/main.rs
@@ -0,0 +1,15 @@
+// #[derive (Debug)]
+
+// enum Direction {North, East, South, West}
+
+// fn main(){
+//     let dir = Direction::East;
+    
+
+//     match dir {
+//         Direction::North => println!("Going North!"),
+//         Direction::East => println!("Going East!"),
+//         Direction::South => println!("Going South!"),
+//         Direction::West => println!("Going West!"),
+//     }
+// }
---
+++ enums/target/debug/enums.d
@@ -0,0 +1,1 @@
+C:\Users\Mayan\Desktop\rust\ 30\ days\enums\target\debug\enums.exe: C:\Users\Mayan\Desktop\rust\ 30\ days\enums\src\main.rs
