40c283be59128238f5731ff3fc83ba9f582d829f
Mayan Pathak <ranupathak1986@gmail.com>
2025-09-23 20:49:49 +0530

    added

--- pattern/src/main.rs
+++ pattern/src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+// fn main(){
+//     let number = 2;
+
+//     match number {
+//     1 => println!("One!"),
+//     2 | 3 => println!("Two or three!"),
+//     4..=10 => println!("Between four and ten!"),
+//     _ => println!("Something else!"),
+// }
+
+// }
+
+
+fn main (){
+    let day = "Saturday";
+    match day {
+        "Monday" => println!("Start of the work week!"),
+        "Friday" => println!("Last day of the work week!"),
+        "Saturday" | "Sunday" => println!("It's the weekend!"),
+        _ => println!("Midweek days are so-so."),
+    }
 }
