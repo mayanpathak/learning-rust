4c93443cc164b8f6d8482b92d7f7fc014788a832
Mayan Pathak <ranupathak1986@gmail.com>
2025-09-24 23:38:19 +0530

    added

--- option/src/main.rs
+++ option/src/main.rs
@@ -10,11 +10,20 @@
 //     println!("Nothing: {:?}", maybe_nothing);
 // }
 
-fn main() {
-    let gift: Option<&str> = Some("Toy Car");
+// fn main() {
+//     let gift: Option<&str> = Some("Toy Car");
 
-    match gift {
-        Some(item) => println!("You got a {}", item),
-        None => println!("The box is empty"),
-    }
+//     match gift {
+//         Some(item) => println!("You got a {}", item),
+//         None => println!("The box is empty"),
+//     }
+// }
+
+
+fn main() {
+    let value = Some(10);
+    println!("Value is: {}", value.unwrap()); // prints 10
+
+    let nothing: Option<i32> = None;
+    // println!("{}", nothing.unwrap()); // 💥 panic!
 }
