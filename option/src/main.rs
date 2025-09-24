ba27026df9cc4c9e39607caeb35743cacb9223a0
Mayan Pathak <ranupathak1986@gmail.com>
2025-09-24 23:47:24 +0530

    added comment

--- option/src/main.rs
+++ option/src/main.rs
@@ -54,17 +54,20 @@
 // }
 
 
-fn divide(a: i32, b: i32) -> Result<i32, &'static str> {
-    if b == 0 {
-        Err("Cannot divide by zero")
-    } else {
-        Ok(a / b)
-    }
-}
+// fn divide(a: i32, b: i32) -> Result<i32, &'static str> {
+//     if b == 0 {
+//         Err("Cannot divide by zero")
+//     } else {
+//         Ok(a / b)
+//     }
+// }
 
-fn main() {
-    match divide(10, 2) {
-        Ok(result) => println!("Result: {}", result),
-        Err(e) => println!("Error: {}", e),
-    }
-}
+// fn main() {
+//     match divide(10, 2) {
+//         Ok(result) => println!("Result: {}", result),
+//         Err(e) => println!("Error: {}", e),
+//     }
+// }
+
+
+
