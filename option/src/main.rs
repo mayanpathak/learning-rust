94058c665b8cf27c2a43891a5678908eaf8dc013
Mayan Pathak <ranupathak1986@gmail.com>
2025-09-24 23:32:29 +0530

    added

--- option/src/main.rs
+++ option/src/main.rs
@@ -2,3 +2,10 @@
 //     println!("Hello, world!");
 // }
 
+fn main() {
+    let maybe_number: Option<i32> = Some(5);
+    let maybe_nothing: Option<i32> = None;
+
+    println!("Number: {:?}", maybe_number);
+    println!("Nothing: {:?}", maybe_nothing);
+}
