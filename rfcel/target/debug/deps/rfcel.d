9337f0f5f83b05905d43e85adadebf08242bd6a4
Mayan Pathak <ranupathak1986@gmail.com>
2025-09-25 23:54:23 +0530

    added

---
+++ rfcel/target/debug/deps/rfcel.d
@@ -0,0 +1,5 @@
+C:\Users\Mayan\Desktop\rust 30 days\rfcel\target\debug\deps\rfcel.d: src\main.rs
+
+C:\Users\Mayan\Desktop\rust 30 days\rfcel\target\debug\deps\rfcel.exe: src\main.rs
+
+src\main.rs:
---
+++ rfcel/target/debug/rfcel.d
@@ -0,0 +1,1 @@
+C:\Users\Mayan\Desktop\rust\ 30\ days\rfcel\target\debug\rfcel.exe: C:\Users\Mayan\Desktop\rust\ 30\ days\rfcel\src\main.rs
