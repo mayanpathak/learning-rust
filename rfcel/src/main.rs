d72cc57c3ff07746a4107353c24a4ba16b627052
Mayan Pathak <ranupathak1986@gmail.com>
2025-10-04 04:15:56 +0530

    added

--- rfcel/src/main.rs
+++ rfcel/src/main.rs
@@ -23,12 +23,29 @@
 
 
 
-use std ::cell::RefCell;
+// use std ::cell::RefCell;
 
+// fn main (){
+//     let data = RefCell::new(10);
+//     {*data.borrow_mut() += 5;}
+// println!("data = {}", *data.borrow());
+
+
+// }
+
+
+
+use std ::{rc::Rc, cell::RefCell};
 fn main (){
-    let data = RefCell::new(10);
-    {*data.borrow_mut() += 5;}
-println!("data = {}", *data.borrow());
+    let counter = Rc::new(RefCell::new(0));
+    let c1 = Rc::clone(&counter);
+    let c2 = Rc::clone(&counter);
 
+
+    *c1.borrow_mut()+=1;
+        *c2.borrow_mut() += 10;
+
+     println!("counter = {}", *counter.borrow()); // prints 11
+    println!("strong_count = {}", Rc::strong_count(&counter)); // prints 3
 
 }
