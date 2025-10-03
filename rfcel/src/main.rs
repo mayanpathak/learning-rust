48038eba05c95defb9af51626b4de005c8e9e311
Mayan Pathak <ranupathak1986@gmail.com>
2025-10-04 04:01:11 +0530

    adde

--- rfcel/src/main.rs
+++ rfcel/src/main.rs
@@ -7,15 +7,15 @@
 //     println!("Hello, world!");
 // }
 
-use std::rc::Rc;
+// use std::rc::Rc;
 
-fn main() {
-    let a = Rc::new(5);
-    println!("value = {}", *a);
-    println!("strong_count = {}", Rc::strong_count(&a));
+// fn main() {
+//     let a = Rc::new(5);
+//     println!("value = {}", *a);
+//     println!("strong_count = {}", Rc::strong_count(&a));
 
-    let b = Rc::clone(&a);
-    println!("after clone, strong_count = {}", Rc::strong_count(&a));
+//     let b = Rc::clone(&a);
+//     println!("after clone, strong_count = {}", Rc::strong_count(&a));
 
-    // both drop at end of scope; when count reaches 0, value is dropped
-}
+//     // both drop at end of scope; when count reaches 0, value is dropped
+// }
