4ec758f1a4fed05be7e27bd4455bb14b7231ced1
Mayan Pathak <ranupathak1986@gmail.com>
2025-10-04 04:04:17 +0530

    added

--- rfcel/src/main.rs
+++ rfcel/src/main.rs
@@ -19,3 +19,16 @@
 
 //     // both drop at end of scope; when count reaches 0, value is dropped
 // }
+
+
+
+
+use std ::cell::RefCell;
+
+fn main (){
+    let data = RefCell::new(10);
+    {*data.borrow_mut() += 5;}
+println!("data = {}", *data.borrow());
+
+
+}
